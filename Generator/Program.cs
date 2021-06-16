83b356783f36b3a8955dc62f6ee24fbb8bba867f
Ростислав Батькович <eminroth@gmail.com>
2021-06-16 18:04:03 +0300

    Initial commit for Generator app

---
+++ Generator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace Generator
+{
+    internal static class Program
+    {
+        private static readonly string[] LtsVersions;
+        private static readonly string[] SupportedVersions;
+        private static readonly string[] AllVersions;
+
+        static Program()
+        {
+            var ltsText = File.ReadAllText("lts.txt");
+            var supportedText = File.ReadAllText("supported.txt");
+            LtsVersions = ltsText.Split(',', StringSplitOptions.RemoveEmptyEntries);
+            SupportedVersions = supportedText.Split(',', StringSplitOptions.RemoveEmptyEntries);
+
+            var versionFiles = Directory.GetFiles("Versions");
+            AllVersions = new string[versionFiles.Length];
+            int i = 0;
+            foreach (var versionFile in versionFiles)
+            {
+                var file = new FileInfo(versionFile);
+                var version = file.Name.Split('-')[1];
+                AllVersions[i++] = version;
+            }
+        }
+        
+        private static void Main()
+        {
+            
+        }
+    }
+}
